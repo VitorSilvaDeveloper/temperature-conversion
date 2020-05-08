b00197a4c9782d55562c1e8400985e672f40f864
VitorSorriso <vitorbackinha@gmail.com>
2020-05-08 02:55:29 -0300

    modified .py, now 100% functional

--- temperature_conversion/temperature-conversion.py
+++ temperature_conversion/temperature-conversion.py
@@ -1,25 +1,44 @@
 def linha():
     print('-'*50)
 
-print('Qual operação deseja fazer?')
-linha()
-print(
+
+def celsius():
+    resultado = (temperatura*1.8) + 32
+    print(f'A temperatura {temperatura:.1f}°C convertida é de {resultado:.1f}°F')
+
+
+def fahrenheit():
+    resultado = (temperatura - 32) / 1.8
+    print(f'A temperatura {temperatura:.1f}°F convertida é de {resultado:.1f}°C')
+
+
+def print_tabela():
+    print(
     '''
     [1] Celsius(°C) para Fahrenheit(°F)
     [2] Fahrenheit(°F) para Celsius(°C)
+    [3] para encerrar o programa
     '''
-)
-linha()
-opcao = int(input('Digite a opção: '))
-linha()
+    )
+    
+while True:
+    linha()
+    print('Qual operação deseja fazer?')
+    linha()
+    print_tabela()
+    linha()
 
-def convertendo():
-    if opcao == 1:
-        temperatura = float(input('Digite a temperatura que quer converter: ')) 
-        resultado = (temperatura*1.8) + 32
-        print(f'A temperatura {temperatura:.1f}°C convertida é de {resultado:.1f}°F')
-    elif opcao == 2:
+    opcao = int(input('Digite a opção: '))
+    if opcao == 1 or opcao == 2:
+        linha()
         temperatura = float(input('Digite a temperatura que quer converter: '))
-        resultado = (temperatura - 32) /1.8
-        print(f'A temperatura {temperatura:.1f}°C convertida é de {resultado:.1f}°F')
-convertendo()
+
+        if opcao == 1:
+            celsius()
+        elif opcao == 2:
+            fahrenheit()
+    elif opcao == 3:
+        print('Finalizando..')
+        break
+    else:
+        print('Comando invalido. ERROR')
